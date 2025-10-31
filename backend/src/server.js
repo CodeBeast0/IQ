608b0118c78ab5ec3ee14c3f03592505bf56493e
CodeBeast0 <161934121+CodeBeast0@users.noreply.github.com>
2025-10-31 22:37:24 +0100

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.cors({ origin: ENV.CLIENT_URL, credential: true })); //credential:true meaning?? => server allows a browser to include cookies on request
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
@@ -15,7 +24,7 @@ const startServer = async () => {
       console.log("Server is running on port:", ENV.PORT);
     });
   } catch (error) {
-    console.error("Error starting the server",error)
+    console.error("Error starting the server", error);
   }
 };
 

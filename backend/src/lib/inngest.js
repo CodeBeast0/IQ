c23276130564dd925fde9f8424df44f0061375b9
CodeBeast0 <161934121+CodeBeast0@users.noreply.github.com>
2025-11-01 15:42:08 +0100

    fixing clerk events

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,43 +1,42 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./db.js"
-import User from "../models/User.js"
+import { connectDB } from "./db.js";
+import User from "../models/User.js";
 
+export const inngest = new Inngest({ id: "talent-iq" });
 
-export const inngest = new Inngest({ id: "talent-iq"});
-
+// Create new Clerk user
 const syncUser = inngest.createFunction(
-    { id:"sync-user"},
-    { event:"clerk/user.created"},
-    async({event}) =>{
-        await connectDB()
+  { id: "sync-user" },
+  { event: "user.created" },
+  async ({ event }) => {
+    await connectDB();
 
-        const {id,email_addresses, first_name,last_name,image_url} = event.data
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-        const newUser = {
-            clerkId:id,
-            email:email_addresses[0]?.email_address,
-            name:`${first_name || ""} ${last_name|| ""}`,
-            profileImage:image_url
-        }
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      profileImage: image_url,
+    };
 
-        await User.create(newUser)
+    await User.create(newUser);
+    console.log("✅ User created in Mongo:", newUser);
+  }
+);
 
-        //todo:do something else
-    }
-)
-
+// Delete Clerk user
 const deleteUserFromDB = inngest.createFunction(
-    { id:"delete-user-from-db"},
-    { event:"clerk/user.deleted"},
-    async({event}) =>{
-        await connectDB()
+  { id: "delete-user-from-db" },
+  { event: "user.deleted" },
+  async ({ event }) => {
+    await connectDB();
 
-        const {id} = event.data
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
 
-        await User.deleteOne({clerkId:id});
+    console.log("🗑️ User deleted from Mongo:", id);
+  }
+);
 
-        //todo:do something else
-    }
-)
-
-export const functions = [syncUser,deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB];

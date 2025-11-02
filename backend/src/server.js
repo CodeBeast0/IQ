6383efcf7a1e1d7efb6777bd1d8d2caba4d044d0
CodeBeast0 <161934121+CodeBeast0@users.noreply.github.com>
2025-11-02 13:55:44 +0100

    fix_all_problem

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,28 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import { serve } from "inngest/express";
-import { inngest,functions } from "./lib/inngest.js";
+import { connectDB } from "./lib/db.js";
+
 
 const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credential: true })); //credential:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credential: true })); 
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-const startServer = async () => {
+const startServer = async()=>{
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log("Server is running on port:", ENV.PORT);
-    });
+    app.listen(ENV.PORT,()=>console.log("Server is running on port:",ENV.PORT));
   } catch (error) {
-    console.error("Error starting the server", error);
+    console.error("Error starting the server",error);
   }
-};
+}
 
 startServer();
